b92ec7e8b9bdd67961e49d57e88f138d9d2b0a1a
Vasu Ramanujam <vasur@umich.edu>
2023-01-23 15:22:55 -0500

    added story one in full.
    separate file with 200 word size slices of text. variables are called as timeline var in experiment.js

--- experiment.js
+++ experiment.js
@@ -1,28 +1,11 @@
-/*
-INSTRUCTIONS FOR RUNNING
 
-- go to correct directory
-$ open experiment.html
-*/
-
-//--- hardcode stories (not as text files)
-var story_one_text = `
-<p>If you were to journey to the North of England, you would come to a valley that is surrounded by moors as high as mountains.
-It is in this valley where you would find the city of Bradford, where once a thousand spinning-jennies that hummed and clattered spun wool into money for the long-bearded mill owners. 
-That all mill owners were generally busy as beavers and quite pleased with themselves for being so successful and well off was known to the residents of Bradford, and if you were to go into the city to visit the stately City Hall, you would see there the Crest of the City of Bradford, which those same mill-owners created to celebrate their achievements. 
-It shows a sinister looking boar's head sitting on top of a well which seems puzzling at first, but the reason for this symbol is a matter of legend. 
-There was once, legend has it, a fearsome boar, which lived in a wood located just outside the manor of Bradford.
-A source of great trouble to the local folk the boar was, bringing terror to the peaceful flocks and ravaging the countryside around. 
-Even worse, however, the boar most liked to go to the well that was in the wood and drink its fresh water, so that the people of Bradford had second thoughts about visiting the well. 
-</p>
-`;
-
-
-//-------
 
 var jsPsych = initJsPsych();
 var timeline = [];
 
+
+
+//1. welcome screen
 const welcome = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: 'Welcome to the experiment! Press any key to begin.'
@@ -31,8 +14,7 @@ timeline.push(welcome);
 
 
 
-//virtual chinrest
-/*
+//2. virtual chinrest
 const get_size = {
     type: jsPsychVirtualChinrest,
     blindspot_reps: 3,
@@ -50,9 +32,9 @@ var resized_stimulus = {
   choices: ['Continue']
 };
 timeline.push(resized_stimulus);
-*/
 
-//instructions
+
+//3. instructions
 const instructions = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
@@ -66,96 +48,31 @@ const instructions = {
 }
 timeline.push(instructions);
 
-//rn display stories as html-keyboard-response. later, add it in as SPR moving window.
-/*
-const story_one_as_html = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: story_one.txt
-}
-timeline.push(story_one_as_html);
-*/
+
 
 //timeline variables of stories. works as hardcoded string
-/*
-var stories = [
-    { story: "story_one.txt" }
+
+//4. display story one
+var story_one = [
+    { part: story_one_one },
+    { part: story_one_two},
+    { part: story_one_three},
+    { part: story_one_four},
+    { part: story_one_five},
+    { part: story_one_six},
+    { part: story_one_seven}
 ];
 
 var display = {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: jsPsych.timelineVariable("story")
+    stimulus: jsPsych.timelineVariable("part")
 }
 
-var display_stories = {
+var display_story_one = {
     timeline: [display],
-    timeline_variables: stories
+    timeline_variables: story_one
 }
-timeline.push(display_stories);
-*/
-
-//file -> input
-//create arr, create func to read input
-/*
-
-var story_one_arr = []
-var read_input_func = function(){
-    const f = require('fs');
-    const readline = require('readline');
-    var user_file = './story_one.txt';
-    var r = readline.createInterface({
-        input: f.createReadStream(user_file)
-    });
-    r.on('line', function (text){
-        story_one_arr.push(text);
-    })    
-}
-*/
-
-//trial to read input from read_input_func
-/*
-var read_input_trial = {
-    type: jsPsychCallFunction,
-    func: read_input_func
-}
-timeline.push(read_input_trial)
-*/
-
-//syncReadFile to read file
-/*
-function syncReadFile(filename) {
-    const contents = readFileSync(filename, 'utf-8');
-  
-    const arr = contents.split(/\r?\n/);
-  
-    console.log(arr); 
-  
-    return arr;
-}
-*/
-
-
-
-//array of words 
-/*
-//var story_one_arr = syncReadFile('story_one.txt');
-var story_one_arr = ["This is the first sentence. Story one part one.", "This is the second sentence. Story one part two.", "This is the third sentence. Story one part three.", story_one_text];
-var counter = 0;
-
-//turn array -> variables
-
-var some_trial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: function(){
-        return "<p>" + story_one_arr[counter++] + "</p>";
-    }
-}
-
-for (let i = 0; i < story_one_arr.length; i++){
-    timeline.push(some_trial);
-}
-*/
-
-
+timeline.push(display_story_one);
 
 
 
