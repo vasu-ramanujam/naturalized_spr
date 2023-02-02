144a53c79951b2529c97ef87d89b9bf1974d4868
Vasu Ramanujam <vasur@umich.edu>
2023-02-02 16:55:17 -0500

    added story_two

--- story_text.js
+++ story_text.js
@@ -55,97 +55,29 @@ The Lord wondered for a moment why he always encountered so much chicanery in hi
 The huntsman who had earned his prize fair and square received as his reward a piece of land just outside the town, known thereafter as Hunt Yard. 
 His fame was indeed assured, but it was not nearly as lasting as that of the fearsome Bradford Boar. `;
 
-
-var story_one_one = `
-<p id="one_one"> If you were to journey to the North of England, you would come to a valley that is surrounded by moors as high as mountains. 
-It is in this valley where you would find the city of Bradford, where once a thousand spinning-jennies that hummed and clattered spun wool into money for the long-bearded mill owners. 
-That all mill owners were generally busy as beavers and quite pleased with themselves for being so successful and well off was known to the residents of Bradford, and if you were to go into the city to visit the stately City Hall, you would see there the Crest of the City of Bradford, which those same mill-owners created to celebrate their achievements. 
-It shows a sinister looking boar's head sitting on top of a well which seems puzzling at first, but the reason for this symbol is a matter of legend. 
-There was once, legend has it, a fearsome boar, which lived in a wood located just outside the manor of Bradford.
-A source of great trouble to the local folk the boar was, bringing terror to the peaceful flocks and ravaging the countryside around. 
-Even worse, however, the boar most liked to 
-</p>
-`;
-
-var story_one_two = `
-<p> go to the well that was in the wood and drink its fresh water, so that the people of Bradford had second thoughts about visiting the well.  If you were to journey to the North of England, you would come to a valley that is surrounded by moors as high as mountains. 
-It is in this valley where you would find the city of Bradford, where once a thousand spinning-jennies that hummed and clattered spun wool into money for the long-bearded mill owners. 
-That all mill owners were generally busy as beavers and quite pleased with themselves for being so successful and well off was known to the residents of Bradford, and if you were to go into the city to visit the stately City Hall, you would see there the Crest of the City of Bradford, which those same mill-owners created to celebrate their achievements. 
-It shows a sinister looking boar's head sitting on top of a well which seems puzzling at first, but the reason for this symbol is a matter of legend. 
-There was once, legend has it, a fearsome boar, which lived in a wood located just outside the manor of Bradford.
-A source of great 
-</p>
-`;
-
-var story_one_three = `
-<p> trouble to the local folk the boar was, bringing terror to the peaceful flocks and ravaging the countryside around. 
-Even worse, however, the boar most liked to go to the well that was in the wood and drink its fresh water, so that the people of Bradford had second thoughts about visiting the well. 
-That the people of Bradford bore the brunt of the beast's ferocity was unfair in the eyes of the people of the region. 
-Eventually, the issue reached the ears of the kindly Lord of the Manor who the people had often asked for help. 
-The Lord saw the severity of the problem the people faced and suggested a contest could solve the problem.
-He said that whoever could kill the boar and bring as proof its head to the Manor House, would be rewarded with land and fame.
-It was the people of Bradford and the people who knew them who rejoiced at this proclamation but one question remained: who would kill the boar? 
-By the handsome reward many felt tempted, but the thought of the boar with its deadly tusks and face like thunder soon put an end to their ambitions. 
-However, there was one 
-</p>
-`;
-var story_one_four = `
-<p> huntsman, who was still wet behind the ears, who decided the prize was worth a shot in spite of the boar's reputation. 
-The huntsman discovered the boar preferred to come out in the middle of the day. 
-So, he went to the wood by the well with his good bow, to bide his time. 
-Around noontime, the boar, feared by the locals, came out, as slow as a snail, from among the trees, just as the huntsman had predicted. 
-The huntsman leaped from his hiding place and through the heart with his fine arrows shot the boar. 
-Now the problem was no longer to kill the boar. 
-It was to find a way to carry the boar back. 
-The head was too heavy for the little huntsman to carry back to the Manor House, but the huntsman, who was as quick-witted and cunning as ever, asked himself, “What else can I do to prove I killed the boar?” 
-The next instance he opened the boar's mouth and cut out its tongue, taking that as proof for the Lord. 
-He set out for the Manor House as quickly as he could and he had only been gone a few minutes when 
-</p>
-`;
-var story_one_five = `
-<p> a second huntsman, not so bold as the first but a little more cunning, came on the scene. 
-Seeing the slain carcass of the boar, the huntsman rejoiced in his good fortune. 
-The second huntsman knew a shortcut to the Manor House was just around the nearby pond, and, being a bigger man than the first, was able to pick up the boar's head and carry it through the wood towards the prize that awaited him. 
-The Lord of the Manor was seated in his hall when the second huntsman burst through the door and began to spin a line. 
-The woes of Bradford are ended! he announced. 
-With my own hand I have slain the boar! 
-And, with that, at the feet of his lord he dropped the boar's enormous head. 
-Then you will be rewarded, as I promised. 
-But first, let me examine the head of this monster, said the Lord of the Manor. 
-To not examine the head in advance had not been very clever on the part of the huntsman. 
-Right away, the Lord noticed the tongue of the boar was missing and exclaimed, "What is this? A boar with no tongue?" 
-He looked at the 
-</p>
-`;
-var story_one_six = `
-<p> huntsman, dubiously. 
-How come this creature has no tongue? he demanded. 
-All eyes were on the huntsman. 
-The huntsman questioned by the Lord replied, "I cannot say, my Lord." 
-He was suddenly realizing his tricky situation could end badly for him. 
-Through the door at this moment, the first huntsman burst. 
-The woes of Bradford are ended! he cried. 
-I have slain the boar the people of Bradford dreaded! 
-All eyes turned to the man who now stood in the doorway. 
-The reward is already given, said the Lord of the Manor. 
-The man here has brought to me the boar's head. 
-Then where is its tongue? the first huntsman replied. 
-And, so saying, he drew out the boar's tongue he had put in his hunting pouch and related how he had ambushed the creature in the wood and cut out its tongue as proof of his victory. 
-Listening to the tale, the Lord of the Manor tried to discern which huntsman was telling him the truth and what the two huntsmen were thinking as they waited for him to make a decision. 
-Almost instantly, the Lord saw that the second huntsman had fed him a pack of lies and 
-</p>
-`;
-
-var story_one_seven = ` <p>
-it was the first huntsman who was the true savior of Bradford. 
-The Lord believed the second huntsman had tried to steal the first huntsman's prize and so proclaimed the first huntsman the true victor. 
-The Lord wondered for a moment why he always encountered so much chicanery in his everyday interactions. 
-The huntsman who had earned his prize fair and square received as his reward a piece of land just outside the town, known thereafter as Hunt Yard. 
-His fame was indeed assured, but it was not nearly as lasting as that of the fearsome Bradford Boar. 
-</p> ` ;
-//--
-
-
-
-//calculate shit
-//var char_space = 1000;
+var story_two = `A clear and joyous day it was and out on the wide, 
+open sea thousands upon thousands of sparkling water-drops excited 
+by getting to play in the ocean danced all around. One of these was 
+a merry little fellow named Aqua who danced on the silver backs of 
+the fishes as they plunged up and down in the waves, and, no matter how high he sprung, he always came down again into his mother's lap. His mother, you know, was the Ocean, and very beautiful she looked that summer day in her dark 
+blue dress and white ruffles. By and by, the happy water-drop tired of his play, and looking up to the clear sky above him, thought he would like to have a sail on one of the white, marshmallow-like clouds, instead of spending his whole life in the ocean. 
+Of the sky, little Aqua had always been afraid, but he 
+decided to finally face his demons and suggested a solution 
+involving the Sun carrying him up to the sky would be ideal. The Sun understood Aqua’s request came from his heart, so he acquiesced and also took ever so many other drops, so that Aqua might not be lonesome on the way. It was only the sun that knew this, however, 
+for all the other drops had been changed into fine mist or vapor and Aqua could not see 
+them. Do you know what vapor is? If you breathe into the air, when it is cold enough, you will see it coming out of your mouth thick and fast like steam. When the vapor is quite near the earth, we call it "fog." Into vapor the water drops that danced 
+in the ocean had been changed because in their own shape they were too heavy for the sunbeams that the sun let out to carry. Higher and higher they sailed, and though it was grand, Aqua and his friends were like fish out of water, so they grew quite 
+dizzy. They enjoyed looking down on the world below, while the wind scattered them across the four corners of the earth, but after a while it grew nearly time for the Sun to 
+go to bed. He became very red in the face, and began to sink lower and lower, until suddenly he went clear out of sight! Poor little Aqua frightened by the impending cold and darkness 
+wished he could sleep not in the sky, but somewhere closer to earth. Then, Aqua heard the wind blow and conjectured that maybe the wind could take him back down to the ocean. Indeed, the wind took pity on him and fanned him, together with all of his brothers and sisters,
+ into a heavy gray cloud, after which he blew them apart and told them to hurry down, fast and furious to the earth so they could have a fighting chance of reaching the ocean. Down they 
+went, rolling over each other until with a patter and clatter and spatter they touched the ground, and all the people that the raindrops fell on cried, "It’s raining", and took out their umbrellas. Some of the drops that were the most fortunate, including Aqua, fell on a mountainside, 
+and down the rocky cliff they ran. Together they plunged into a mountain brook, 
+which came foaming and dashing along, leaping over rocks and rushing down the hillside, until in the valley below they heard the strangest clattering noise. The little water-drops were then able 
+to make out a flourmill that was standing on the bank. Inside the mill the water-drops saw two great stones, which the big wheel kept whizzing round and round. The stones smoothed by the spinning, were busy grinding the wheat into flour and the corn into golden meal. But what giant 
+do you suppose it was who could turn and swing that tremendous wheel? No giant at all. It was just our tiny little water drops, who sprang on it by hundreds and thousands, and whirled it over and over. 
+It was lucky for the water-drops that the brook where they were swimming next emptied into a beautiful pond where ducks and geese were living peacefully. Aqua liked to play hide and seek in this pond, and it was his brother water drop who he fooled by swimming to a big pool of water and pretending to hide there, but then sneaking under a rock into an obscured little puddle. 
+His brother had blatantly peaked and even followed Aqua as he entered the big pool of water, so when he searched there and could not find him, he was extremely surprised and gave up. Aqua liked this pond where he could play all day, so he stayed a good while, sailing up and down, taking the ducks' backs for ships and the frogs for horses, but after a time he tired of the dull life, and he and his brothers 
+floated under a bridge for a long distance, until they saw another brook tumbling down a hillside. Come, let's join hands! cried Aqua and because the water drops were all together, overcoming the brook was child’s play. It was by the help of Aqua and his brothers that the bewitching river was able to float heavy ships, though not so long ago it was only a little stream. As the river grew wider and he could 
+almost see the ocean, Aqua realized his journey was almost over and he had come full circle. Back in the ocean, he felt right at home. For him, this was the safest, sweetest place in the whole wide world. During the warm summer evenings, if you take a walk on the sea beach, you will hear the gentle rippling swash of the waves and some say that it must be the gurgling voices 
+of Aqua and his brother water drops telling each other about their wonderful journey around the world.`;
